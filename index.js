feb70aaf61bd0bb1dd4dd58091416775fe3c34be
FollowNaim <naimxf@gmail.com>
2024-12-22 19:59:42 +0600

    feat: implemented booking and review endpoints with advanced features - Added endpoint to retrieve all bookings - Added endpoint to fetch specific booking details by ID - Created endpoint to retrieve user-specific bookings based on user ID - Implemented feature to increase review count simultaneously on two collections in the database - Updated review endpoint to handle dynamic updates and ensure consistency
    Signed-off-by: FollowNaim <naimxf@gmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://learnmates.vercel.app", "http://localhost:5173"],
+  })
+);
 app.use(express.json());
 
 // mongo url
@@ -32,6 +36,7 @@ async function run() {
     const db = client.db("learnMate");
     const usersCollection = db.collection("users");
     const tutorsCollection = db.collection("tutors");
+    const bookingsCollection = db.collection("bookings");
     // save per user to db
     app.post("/user", async (req, res) => {
       const user = req.body;
@@ -42,14 +47,16 @@ async function run() {
     // get all tutors
     app.get("/tutors", async (req, res) => {
       const category = req.query.category;
+
       let query = {};
       if (category) {
         query.category = category;
       } else {
         query = {};
       }
+
       const result = await tutorsCollection.find(query).toArray();
-      console.log(result, query);
+
       res.send(result);
     });
     // get all categories
@@ -69,6 +76,68 @@ async function run() {
     app.post("/tutors", async (req, res) => {
       const tutor = req.body;
       const result = await tutorsCollection.insertOne(tutor);
+      res.send(result);
+    });
+    // update a tutorial
+    app.put("/tutors/:id", async (req, res) => {
+      const id = req.params.id;
+      const details = req.body;
+      const result = await tutorsCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: details,
+        },
+        { upsert: true }
+      );
+      res.send(result);
+    });
+    // update reveiw
+    app.patch("/update-review/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await tutorsCollection.updateMany(
+        { _id: new ObjectId(id) },
+        { $inc: { review: 1 } }
+      );
+      const result2 = await bookingsCollection.updateMany(
+        { tutorId: id },
+        { $inc: { review: 1 } }
+      );
+      console.log(id);
+      console.log(result);
+      console.log(result2);
+      res.send(result);
+    });
+    // get my tutorials
+    app.get("/my-tutorials/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await tutorsCollection.find({ email }).toArray();
+      res.send(result);
+    });
+
+    // get all booking data
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) query.email = email;
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // save booking data
+    app.post("/bookings", async (req, res) => {
+      const details = req.body;
+      const exists = await bookingsCollection.findOne({
+        email: details.email,
+        tutorId: details.tutorId,
+      });
+      if (exists)
+        return res.status(409).send("The data already exists in the database.");
+      const prev = await tutorsCollection.findOne({
+        _id: new ObjectId(details.tutorId),
+      });
+      details.review = prev.review;
+      const result = await bookingsCollection.insertOne(details);
       res.send(result);
     });
   } catch (err) {
