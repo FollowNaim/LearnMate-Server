7d56252bbfd285396406528eb8c22829582ccc68
FollowNaim <naimxf@gmail.com>
2024-12-22 12:29:25 +0600

    feat: implemented APIs for user registration and tutor management - Added API to save user data to DB during registration - Created endpoint to fetch all tutors - Created endpoint to fetch a single tutor by ID - Added endpoint to fetch all categories - Implemented category-based filtering with query parameters
    Signed-off-by: FollowNaim <naimxf@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -21,10 +21,56 @@ const client = new MongoClient(uri, {
   },
 });
 
+app.get("/", (req, res) => {
+  res.send("LearnMate server is running successfully!");
+});
+
 async function run() {
   try {
     client.connect();
     console.log("mongodb connected successfully");
+    const db = client.db("learnMate");
+    const usersCollection = db.collection("users");
+    const tutorsCollection = db.collection("tutors");
+    // save per user to db
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const exists = await usersCollection.findOne({ email: user.email });
+      if (exists) return;
+      await usersCollection.insertOne(req.body);
+    });
+    // get all tutors
+    app.get("/tutors", async (req, res) => {
+      const category = req.query.category;
+      let query = {};
+      if (category) {
+        query.category = category;
+      } else {
+        query = {};
+      }
+      const result = await tutorsCollection.find(query).toArray();
+      console.log(result, query);
+      res.send(result);
+    });
+    // get all categories
+    app.get("/tutors/categories", async (req, res) => {
+      const result = await tutorsCollection
+        .find({}, { projection: { _id: 0, category: 1 } })
+        .toArray();
+      res.send(result);
+    });
+    // get signle tutorial
+    app.get("/tutors/:id", async (req, res) => {
+      const id = new ObjectId(req.params.id);
+      const result = await tutorsCollection.findOne({ _id: id });
+      res.send(result);
+    });
+    // save per tutorial on db
+    app.post("/tutors", async (req, res) => {
+      const tutor = req.body;
+      const result = await tutorsCollection.insertOne(tutor);
+      res.send(result);
+    });
   } catch (err) {
     console.log(err);
   }
